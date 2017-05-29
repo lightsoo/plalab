8ca7914f2083f65fa99a9c3bc3878d34b621c63c
김경수(Kyungsu Kim)/Platform Architecture팀/SKP <lightsoo@sk.com>
2017-05-29 11:35:55 +0900

    Add RequestDTO

---
+++ src/main/java/com/plalab/api/request/MemberRequest.java
@@ -0,0 +1,53 @@
+package com.plalab.api.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MemberRequest {
+    private String name;
+    private Integer age;
+
+//    private MemberRequest(String name, Integer age){
+//        this.name = name;
+//        this.age = age;
+//    }
+
+    public static class Builder{
+        private String name;
+        private Integer age;
+
+        private Builder(String name, Integer age){
+            this.name = name;
+            this.age = age;
+        }
+
+        public Builder setName(String name){
+            this.name = name;
+            return this;
+        }
+
+        public Builder setAge(Integer age){
+            this.age = age;
+            return this;
+        }
+
+        public static Builder newBuilder(String name, Integer age){
+            return new Builder(name, age);
+        }
+
+        public MemberRequest build() {
+            if(this.name == null){
+                throw new IllegalStateException("Name of MemberRequest is required field");
+            }
+            if(this.age == null){
+                throw new IllegalStateException("Age of MemberRequest is required field");
+            }
+
+            return new MemberRequest(this.name, this.age);
+        }
+    }
+
+}
